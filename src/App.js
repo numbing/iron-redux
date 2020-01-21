0108bc01446e779368cc14a32c56a1c81fb6a832
mir <mirnumbing@gmail.com>
2020-01-21 20:05:17 +0100

    create store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
+import store from "store";
 
 function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <div className="App">
         <h1>redux</h1>
       </div>
