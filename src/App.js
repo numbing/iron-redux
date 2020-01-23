654b69b01a5a9e4cf6b4a6b5e770923548352b37
mir <mirnumbing@gmail.com>
2020-01-23 20:17:23 +0100

    carddd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import StarWars from "./components/starWars/starWars";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <h1>redux</h1>
+        <StarWars />
       </div>
     </Provider>
   );
